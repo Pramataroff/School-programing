54b72582ea2fb2aacf9f88dba1160feb28f27999
Milenski <milenpramatarov@gmail.com>
2024-12-05 16:05:50 +0200

    pushing

---
+++ Domashna 06.12.24/Zadacha skobi/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Zadacha_skobi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the text: ");
+            string input = Console.ReadLine();
+            char[] characters = input.ToArray();
+
+
+            Stack<char> stack = new Stack<char>(characters);
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                if (characters[i] == '(' || characters[i] == ')')
+                {
+                    stack.Push(characters[i]);
+                }
+            }
+
+            if (stack.Count % 2 == 0)
+            {
+                Console.WriteLine("skobite sa ravni");
+            }
+            else
+            {
+                Console.WriteLine("Skobite ne sa ravni");
+            }
+        }
+    }
+}
+
+
