23c26492d5505c32ac13f8fa46138155e0d6b3ad
Milenski <milenpramatarov@gmail.com>
2025-01-05 22:46:34 +0200

    update

---
+++ Zadacha 2 02.12.24/Palindrom/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Palindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char[] chars = word.ToCharArray();
+            Stack<char> stack = new Stack<char>(chars);
+            Array.Reverse(chars);
+            Stack<char> reversedstack = new Stack<char>(chars);
+
+            Print(stack, reversedstack);
+        }
+
+        static void Print(Stack<char> stack, Stack<char> reversedstack)
+        {
+            for (int i = 0; i < stack.Count; i++)
+            {
+                if (stack.Pop() != reversedstack.Pop())
+                {
+                    Console.WriteLine("The word is not palindrome");
+                    break;
+                }
+                if (i == stack.Count)
+                {
+                    Console.WriteLine("The word is palindrome");
+                }
+            }
+        }
+    }
+}
