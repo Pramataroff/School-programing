daf6f4acaab74d1565477dc05631819fd491f9d4
Milenski <milenpramatarov@gmail.com>
2025-01-07 16:06:21 +0200

    homework 1/7/25

---
+++ Opashka na magazin/Opashka na magazin/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Opashka_na_magazin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "exit")
+            {
+                switch (command)
+                {
+                    case "add":
+                        {
+                            Console.Write("Enter name: ");
+                            string adder = Console.ReadLine();
+                            queue.Enqueue(adder);
+                            Console.WriteLine($"{adder} is last in the queue");
+                            break;
+                        }
+                    case "print":
+                        {
+                            Console.WriteLine($"current queue: {string.Join(" < ", queue)}");
+                            break;
+                        }
+                    case "serve":
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} was served");
+                            break;
+                        }
+                }
+            }
+            Console.WriteLine("You closed the program");
+        }
+    }
+}
