698a267505dacbebf9f839d767969fc28012dee9
Milenski <milenpramatarov@gmail.com>
2025-02-16 23:09:54 +0200

    upload

---
+++ Homework 16.02.25/Array list bubble sort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+
+namespace Array_list_bubble_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the elements: ");
+            ArrayList array = new ArrayList(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray());
+            int result = 0;
+
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (Int32.TryParse((string)array[i], out result))
+                {
+                    array[i] = (int.Parse((string)array[i]));
+                }
+                else
+                {
+                    int temp = array[i].ToString().Length;
+                    array[i] = temp;
+                }
+            }
+
+            Print(array);
+
+            foreach (var VARIABLE in array)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+        static void Print(ArrayList arrList)
+        {
+            for (int i = 0; i < arrList.Count; i++)
+            {
+                for (int j = 0; j < arrList.Count - 1; j++)
+                {
+                    if ((int)arrList[j] > (int)arrList[j + 1])
+                    {
+                        int swap = (int)arrList[j];
+                        arrList[j] = arrList[j + 1];
+                        arrList[j + 1] = swap;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Homework 16.02.25/Linked list bubble sort/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Linked_list_bubble_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            BubbleSort(linkedList);
+            Console.WriteLine($"Bubble Sorted List: "+ string.Join(", ",linkedList));
+        }
+        static void BubbleSort(LinkedList<int> list)
+        {
+            bool swapped = true;
+            while (swapped)
+            {
+                swapped = false;
+                LinkedListNode<int> current = list.First;
+
+                while (current != null && current.Next != null)
+                {
+                    if (current.Value > current.Next.Value)
+                    {
+                        int temp = current.Value;
+                        current.Value = current.Next.Value;
+                        current.Next.Value = temp;
+                        swapped = true;
+                    }
+                    current = current.Next;
+                }
+            }
+        }
+    }
+}
